53bfe1c2e740f9cf1226b4ed35927554d498301d
David McConnell <mcconnelld01+ncpe@gmail.com>
2026-02-09 17:02:56 +0000

    additional diagnostic plots using MVN package

--- FullExample/check_mvn.R
+++ FullExample/check_mvn.R
@@ -13,4 +13,61 @@ is.models2[[2]] <- is.models[[3]]
 is.models2[[3]] <- is.models[[4]]
 is.models2[[4]] <- is.models[[5]]
 is.models2[[5]] <- is.models[[6]]
-lapply(is.models2, check_mvn_wrapper, log = "xy")  
+lapply(is.models2, check_mvn_wrapper , log="xy"      )  
+
+
+
+## Other diagnostic plots (for two param models only)
+
+is.models3<-is.models
+
+for(i in 1:6)
+{
+  sample.w<-sample(1:length(is.models3[[i]]$sample_weights),
+                   replace=TRUE,
+                   size=5000,
+                   prob = is.models3[[i]]$sample_weights)
+  
+  is.models3[[i]]$mvn_resamples<-is.models3[[i]][["raw_samples_mvn"]][sample.w,]
+  
+}
+
+# Exponential
+
+plot(density(is.models3[[1]]$mvn_resamples))
+
+# 3d density plots for two parameter models
+
+library(MVN)
+
+res2<-mvn(is.models3[[2]]$mvn_resamples)
+res3<-mvn(is.models3[[3]]$mvn_resamples)
+res4<-mvn(is.models3[[4]]$mvn_resamples)
+res5<-mvn(is.models3[[5]]$mvn_resamples)
+
+plot(res2, diagnostic = "multivariate", type = "persp")
+plot(res2, diagnostic = "multivariate", type = "contour")
+plot(res2, diagnostic = "multivariate", type = "qq")
+
+plot(res3, diagnostic = "multivariate", type = "persp")
+plot(res3, diagnostic = "multivariate", type = "contour")
+plot(res3, diagnostic = "multivariate", type = "qq")
+
+
+plot(res4, diagnostic = "multivariate", type = "persp")
+plot(res4, diagnostic = "multivariate", type = "contour")
+plot(res4, diagnostic = "multivariate", type = "qq")
+
+
+plot(res5, diagnostic = "multivariate", type = "persp")
+plot(res5, diagnostic = "multivariate", type = "contour")
+plot(res5, diagnostic = "multivariate", type = "qq")
+
+# Gen gamma - I think only QQ plots are sensible here
+
+res6<-mvn(is.models3[[6]]$mvn_resamples)
+
+plot(res6, diagnostic = "multivariate", type = "qq")
+
+
+
